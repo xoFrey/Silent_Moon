8f3b4eb0438ba4988c09c2336c4bf27769aae6f4
xofrey <xizelacar@gmail.com>
2024-06-14 17:55:18 +0200

    setup index in backened

---
+++ Backend/src/index.js
@@ -0,0 +1,39 @@
+import express from "express";
+import cors from "cors";
+import multer from "multer";
+import mongoose from "mongoose";
+import morgan from "morgan";
+import cookieSession from "cookie-session";
+import "dotenv/config";
+
+const app = express();
+const PORT = process.env.PORT;
+
+const twoWeeksInMs = 14 * 24 * 60 * 60 * 1000;
+const isFrontendLocalhost =
+  process.env.FRONTEND_URL.startsWith("http://localhost");
+const cookieSessionSecret = process.env.COOKIE_SESSION_SECRET;
+
+app.use(cors({ origin: process.env.FRONTEND_URL, credentials: true }));
+app.set("trust proxy", 1);
+const cookieSessionOptions = {
+  name: "session",
+  secret: cookieSessionSecret,
+  httpOnly: true,
+  expires: new Date(Date.now() + twoWeeksInMs),
+  sameSite: isFrontendLocalhost ? "lax" : "none",
+  secure: isFrontendLocalhost ? false : true,
+};
+app.use(cookieSession(cookieSessionOptions));
+
+app.use(morgan("dev"));
+app.use(express.json());
+app.use(express.static("uploads"));
+
+// const upload = multer({ dest: "./uploads" });
+// app.post("/api/v1/files/upload", upload.single("pictures"), (req, res) => {
+//   res.json({ imgUrl: req.file.filename });
+// });
+
+await mongoose.connect(process.env.MONGO_URL, { dbName: "SilentMoon" });
+app.listen(PORT, () => console.log("Server ready at", PORT));
---
+++ Backend/src/user/user.services/user.index.js
@@ -0,0 +1,6 @@
+// export const UserService = {
+//   registerUser,
+//   loginUser,
+//   verifyUser,
+
+// };
