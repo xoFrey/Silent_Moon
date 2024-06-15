a3bf1cc36d0c433adead38c92bc25dbe5613bc31
xofrey <xizelacar@gmail.com>
2024-06-15 07:28:49 +0200

    tailwind config done

--- Frontend/tailwind.config.js
+++ Frontend/tailwind.config.js
@@ -2,6 +2,15 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    colors: {
+      white: "#ffffff",
+      pink: " #E28F83",
+      green: "#8E9775",
+      maintext: "#3F414E",
+      subtext: "#EBEAEC",
+      icon: "#A1A4B2",
+      circle: "#FAF2DA",
+    },
     extend: {},
   },
   plugins: [],
