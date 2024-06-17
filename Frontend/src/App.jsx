bc1885c0158f2eae4431b9fb3b457786662bcece
StefanHolz1997 <Stefanholz97@web.de>
2024-06-17 15:04:49 +0200

    Navbar finished

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import './App.css';
+import "./App.css";
 import Home from "./pages/Home";
 import LandingPage from "./pages/LandingPage";
 import Login from "./pages/Login";
@@ -12,27 +12,33 @@ import YogaPage from "./pages/YogaPage";
 import MeditationPage from "./pages/MeditationPage";
 import MusicPage from "./pages/MusicPage";
 import ProfilePage from "./pages/ProfilePage";
+import Navbar from "./components/Navbar";
 
 function App() {
-  return <>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<LandingPage />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Registration />} />
-        <Route path="/get-started" element={<GetStarted />} />
-        <Route path="/notification" element={<Notification />} />
-        <Route path="/home" element={<Home />} />
-        <Route path="/yoga" element={<YogaPage />} />
-        <Route path="/yoga/:yogaId" element={<YogaDetail />} />
-        <Route path="/meditation" element={<MeditationPage />} />
-        <Route path="/meditation/:meditationId" element={<MeditationDetail />} />
-        <Route path="/music" element={<MusicPage />} />
-        <Route path="/profile" element={<ProfilePage />} />
-      </Routes>
-    </BrowserRouter>
-  </>;
-
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Registration />} />
+          <Route path="/get-started" element={<GetStarted />} />
+          <Route path="/notification" element={<Notification />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/yoga" element={<YogaPage />} />
+          <Route path="/yoga/:yogaId" element={<YogaDetail />} />
+          <Route path="/meditation" element={<MeditationPage />} />
+          <Route
+            path="/meditation/:meditationId"
+            element={<MeditationDetail />}
+          />
+          <Route path="/music" element={<MusicPage />} />
+          <Route path="/profile" element={<ProfilePage />} />
+        </Routes>
+        <Navbar />
+      </BrowserRouter>
+    </>
+  );
 }
 
 export default App;
--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -1,7 +1,11 @@
+import Navbar from "../components/Navbar";
+
 const Home = () => {
-    return <>
-        <h1>Home</h1>
-    </>;
+  return (
+    <>
+      <h1>Home</h1>
+    </>
+  );
 };
 
 export default Home;
