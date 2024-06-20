cbf1a85e90d13237fd5cd197c023d893b6da492d
ChrissQAng <goodomen@outlook.de>
2024-06-20 10:45:37 +0200

    detail yoga

--- Frontend/src/pages/MeditationDetail.jsx
+++ Frontend/src/pages/MeditationDetail.jsx
@@ -1,7 +1,32 @@
+import { useParams } from "react-router-dom";
+import ButtonLike from "../components/ButtonLike";
+import { useContext, useEffect, useState } from "react";
+import { backendUrl } from "../api/api";
+import { TokenContext } from "../../context/Context";
+import ButtonDownload from "../components/ButtonDownload";
+
 const MeditationDetail = () => {
-    return <>
-        <h1>Meditation Detail</h1>
-    </>;
+    const { yogaId } = useParams();
+    const [oneYoga, setOneYoga] = useState({});
+    const { token } = useContext(TokenContext);
+  
+    useEffect(() => {
+      const fetchYoga = async () => {
+        const res = await fetch(`${backendUrl}/api/v1/yoga/detail/${yogaId}`);
+        // , {
+        //   headers: { authorization: `Bearer ${token}` },
+        // }
+        const data = await res.json();
+        if (!data.result) return "Failed to fetch one Yoga";
+        setOneYoga(data?.result);
+      };
+      fetchYoga();
+    }, []);
+  return (
+    <>
+      <h1>Meditation Detail</h1>
+    </>
+  );
 };
 
 export default MeditationDetail;
