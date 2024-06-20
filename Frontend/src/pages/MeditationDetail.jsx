c91ccce63cbe5c5f2c5f7bdc6aabb774a0ee2921
ChrissQAng <goodomen@outlook.de>
2024-06-20 10:52:32 +0200

    update

--- Frontend/src/pages/MeditationDetail.jsx
+++ Frontend/src/pages/MeditationDetail.jsx
@@ -6,22 +6,22 @@ import { TokenContext } from "../../context/Context";
 import ButtonDownload from "../components/ButtonDownload";
 
 const MeditationDetail = () => {
-    const { yogaId } = useParams();
-    const [oneYoga, setOneYoga] = useState({});
-    const { token } = useContext(TokenContext);
-  
-    useEffect(() => {
-      const fetchYoga = async () => {
-        const res = await fetch(`${backendUrl}/api/v1/yoga/detail/${yogaId}`);
-        // , {
-        //   headers: { authorization: `Bearer ${token}` },
-        // }
-        const data = await res.json();
-        if (!data.result) return "Failed to fetch one Yoga";
-        setOneYoga(data?.result);
-      };
-      fetchYoga();
-    }, []);
+  const { meditationId } = useParams();
+  const [oneYoga, setOneYoga] = useState({});
+  const { token } = useContext(TokenContext);
+
+  useEffect(() => {
+    const fetchYoga = async () => {
+      const res = await fetch(`${backendUrl}/api/v1/yoga/detail/${yogaId}`);
+      // , {
+      //   headers: { authorization: `Bearer ${token}` },
+      // }
+      const data = await res.json();
+      if (!data.result) return "Failed to fetch one Yoga";
+      setOneYoga(data?.result);
+    };
+    fetchYoga();
+  }, []);
   return (
     <>
       <h1>Meditation Detail</h1>
