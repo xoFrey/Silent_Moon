934800ff34579e9eb5f5bfe2e4ad05afb546252a
xofrey <xizelacar@gmail.com>
2024-06-22 09:45:19 +0200

    guest user, videoURL for later, editYoga

--- Backend/src/yoga/yoga.routes.js
+++ Backend/src/yoga/yoga.routes.js
@@ -10,4 +10,5 @@ export const yogaRoutes = express
   .get("/filterLevel/", YogaController.getYogasByLevelCtrl) // example fetch: api/v1/yoga/filterLevel/?levelSelection=Beginner
   .get("/filterCategory/", doJwtAuth, YogaController.getYogasByCategoryCtrl) // example fetch: api/v1/yoga/filterCategory/?categorySelection=Sleep
   .get("/detail/:yogaId", YogaController.getYogaDetailCtrl)
-  .post("/", upload.single("files"), YogaController.createYogaCtrl);
+  .post("/", upload.single("files"), YogaController.createYogaCtrl)
+  .patch("/:yogaId", upload.single("files"), YogaController.ed);
