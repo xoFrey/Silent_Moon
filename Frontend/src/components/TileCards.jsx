5b4a38bdeb70f00a4d7e22c8bdcc1e610a4fe214
StefanHolz1997 <Stefanholz97@web.de>
2024-06-24 11:03:20 +0200

    public url removed, img adjusted

--- Frontend/src/components/TileCards.jsx
+++ Frontend/src/components/TileCards.jsx
@@ -5,20 +5,22 @@ const TileCards = ({ name, imgURL, level, duration, id }) => {
   const [categoryData, setCategoryData] = useState();
   return (
     <section className="flex flex-row gap-5 pl-5 mb-1 ">
-
-      <div>
-        <img src={imgURL || "/image/Fatburner.png"} id="imageCategory" />
+      <div className="w-40">
+        <img
+          src={imgURL || "/image/Fatburner.png"}
+          id="imageCategory"
+          className="h-28 w-40"
+        />
         <label htmlFor="imageCategory">
           <h3 className="leading-5 font-black text-lg text-maintext mt-2 mb-1">
             {name}
           </h3>
         </label>
-        <div className="flex gap-16 text-subtext font-semibold text-xs leading-3">
+        <div className="flex place-content-between text-subtext font-semibold text-xs leading-3">
           <p>{level}</p>
           <p>{duration}</p>
         </div>
       </div>
-
     </section>
   );
 };
