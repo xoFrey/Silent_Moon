8d936b32dd058d80a321581974b33f1068097bad
xofrey <xizelacar@gmail.com>
2024-06-24 13:28:11 +0200

    cloudinary in meditation

--- Backend/src/meditation/meditation.services/meditation.createMeditation.js
+++ Backend/src/meditation/meditation.services/meditation.createMeditation.js
@@ -1,6 +1,18 @@
+import { uploadImage } from "../../utils/uploads.js";
 import { Meditation } from "../meditation.model.js";
 
 export const createMeditation = async (meditationInfo) => {
+  if (meditationInfo.fileUrl) {
+    const uploadResult = await uploadImage(
+      meditationInfo.fileUrl.buffer,
+      "files",
+    );
+    meditationInfo.fileUrl = uploadResult.secure_url;
+  } else {
+    meditationInfo.fileUrl = user.fileUrl;
+  }
+
   const createdMeditation = await Meditation.create(meditationInfo);
+  console.log(createdMeditation);
   return createdMeditation;
 };
