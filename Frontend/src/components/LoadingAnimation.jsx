72d2514b74a8acc5aca6616dd07001b6b4db67ff
ChrissQAng <goodomen@outlook.de>
2024-06-25 18:16:28 +0200

    guest styling

--- Frontend/src/components/LoadingAnimation.jsx
+++ Frontend/src/components/LoadingAnimation.jsx
@@ -1,15 +1,13 @@
 import React from "react";
-import { DotLottieReact } from "@lottiefiles/dotlottie-react";
 
 const LoadingAnimation = () => {
   return (
     <>
-      {/* <DotLottieReact
-        src="https://lottie.host/20d5a0fd-e589-4a5c-af13-1e4ed2e57144/QUfMJvu215.json"
-        loop
-        autoplay
-      /> */}
-      <img className=" animate-pulse" src="../../image/yoda.png" alt="yoda" />
+      <img
+        className=" w-screen max-w-48 yoda "
+        src="../../image/yoda3.png"
+        alt="yoda"
+      />
     </>
   );
 };
--- Frontend/src/components/VerificationPopUp.jsx
+++ Frontend/src/components/VerificationPopUp.jsx
@@ -29,50 +29,53 @@ const VerficationPopUp = ({ setShowVerification, showVerification }) => {
     navigate("/notification");
   };
 
-  return <section className="w-screen">
-    <div className=" h-full fixed bg-maintext/85 top-0 w-screen z-10 md:px-24 lg:px-48 px-6   ">
-      <div className="bg-white rounded-lg sticky top-20 ">
-        <section className="flex flex-col">
-          <div className=" bg-circle h-2 rounded-t-lg mb-4"></div>
-          <div>
-            <h1 className="font-black text-maintext text-2xl leading-10 text-center tracking-wide mb-2">
-              Verifiy your email
-            </h1>
-            <p className="text-subtext text-lg  text-center font-semibold mx-4">
-              Hi {user?.firstname}, Please type in the 6-Digit Code we've sent
-              you to:{" "}
-              <span className="font-bold text-maintext">
-                {user?.email}
-              </span>
+  return (
+    <section className="w-screen">
+      <div className=" h-full fixed bg-maintext/85 top-0 w-screen z-10 md:px-24 lg:px-48 px-6   ">
+        <div className="bg-white rounded-lg sticky top-20 ">
+          <section className="flex flex-col">
+            <div className=" bg-circle h-2 rounded-t-lg mb-4"></div>
+            <div>
+              <h1 className="font-black text-maintext text-2xl leading-10 text-center tracking-wide mb-2">
+                Verifiy your email
+              </h1>
+              <p className="text-subtext text-lg  text-center font-semibold mx-4">
+                Hi {user?.firstname}, Please type in the 6-Digit Code we've sent
+                you to:{" "}
+                <span className="font-bold text-maintext">{user?.email}</span>
+              </p>
+            </div>
+            <div className="flex flex-col items-center mt-4 mb-2">
+              <input
+                type="number"
+                maxLength="6"
+                min="6"
+                max="6"
+                onChange={(e) => setSixDigitCode(e.target.value)}
+                className="h-10 w-10/12 border-solid border border-pink text-subtext rounded-lg text-center font-semibold leading-5 tracking-wider mb-2"
+              />
+              <button
+                className="h-10 w-10/12 bg-pink text-circle rounded-lg"
+                onClick={verificateEmail}
+              >
+                Verify
+              </button>
+            </div>
+            <p className="text-subtext leading-5 text-xs text-center ">
+              Questions? Email us as soons as our Support goes Online 😉
             </p>
-          </div>
-          <div className="flex flex-col items-center mt-4 mb-2">
-            <input
-              type="number"
-              maxLength="6"
-              min="6"
-              max="6"
-              onChange={(e) => setSixDigitCode(e.target.value)}
-              className="h-10 w-10/12 border-solid border border-pink text-subtext rounded-lg text-center font-semibold leading-5 tracking-wider mb-2"
-            />
             <button
-              className="h-10 w-10/12 bg-pink text-circle rounded-lg"
-              onClick={verificateEmail}
+              onClick={() => setShowVerification(false)}
+              className="text-subtext leading-5 text-xs text-center mb-6 font-bold cursor-pointer "
             >
-              Verify
+              I will verify later
             </button>
-          </div>
-          <p className="text-subtext leading-5 text-xs text-center ">
-            Questions? Email us as soons as our Support goes Online 😉
-          </p>
-          <button onClick={() => setShowVerification(false)} className="text-subtext leading-5 text-xs text-center mb-6 font-bold cursor-pointer ">
-            I will verify later
-          </button>
-
-        </section>
+            <p className="font-bold text-red text-center ">{errorMessage}</p>
+          </section>
+        </div>
       </div>
-    </div>
-  </section>;
+    </section>
+  );
 };
 
 export default VerficationPopUp;
